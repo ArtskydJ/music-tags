f1e025796239b1159fe51afdb98bb48460b7a9bb
Joseph Dykstra <josephdykstra@gmail.com>
2015-04-15 22:35:18 -0500

    updated deps

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 var musicMetadata = require('musicmetadata')
-var once = require('onetime')
 
-module.exports = function getTagData(stream, opts, callback) {
-	if (typeof opts === 'function') callback = opts
-	var cb = once(callback || function() {})
-	var metadataOpts = (opts && opts.size) ?
-		{duration: true, fileSize: opts.size} : {}
-	var tagData = musicMetadata(stream, metadataOpts)
-	
-	var to = setTimeout(cb.bind(null, new Error('timeout')), 10000)
-	to.unref && to.unref()
-	tagData.once('metadata', cb.bind(null, null))
-	tagData.once('done', cb)
+module.exports = function getTagData(stream, opts, cb) {
+	if (typeof opts === 'function') cb = opts
+	var options = (opts && opts.size) ? { duration: true, fileSize: opts.size } : {}
+	musicMetadata(stream, options, cb)
 }
